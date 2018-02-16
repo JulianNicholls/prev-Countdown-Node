f31330c887a792ad0d88cdefb89a1fe75d8f96a7
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-16 15:39:58 +0000

    Correct checking of canBeMadeFrom
    Add capability to test, Word, Letter Map, and strings

--- src/Word.js
+++ src/Word.js
@@ -3,19 +3,28 @@
 
 class Word {
   constructor(word) {
-    this.word = word.toLowerCase();
+    this.text = word.toLowerCase();
 
     this._deconstruct();
   }
 
+  // Takes either a String, Word, or Letter Map
   canBeMadeFrom = other => {
-    if (other instanceof Word) other = other.letterMap;
+    if (other instanceof Word) {
+      other = other.letterMap;
+    } else if (!(other instanceof Map)) {
+      other = new Word(other).letterMap;
+    }
+
+    let ok = true;
 
     this.letterMap.forEach((value, key) => {
-      if (!other.has(key) || other.get(key) < value) return false;
+      if (!other.has(key) || other.get(key) < value) {
+        ok = false;
+      }
     });
 
-    return true;
+    return ok;
   };
 
   isAnagramOf = other => {
@@ -35,7 +44,7 @@ class Word {
   _deconstruct = () => {
     this.letterMap = new Map();
 
-    this.word.split('').forEach(char => {
+    this.text.split('').forEach(char => {
       const count = this.letterMap.get(char);
 
       this.letterMap.set(char, count ? count + 1 : 1);
