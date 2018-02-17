e41b72f27e5add53ab50f11ad537d07953752ea8
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-17 17:07:43 +0000

    Add bare bones word list output

--- src/components/WordView.js
+++ src/components/WordView.js
@@ -1,6 +1,32 @@
 import React from 'react';
 
+const WordBox = ({ words }) => (
+  <div className="word-box">{words.map(word => <span key="word">{word}</span>)}</div>
+);
+
 class WordView extends React.Component {
+  renderBoxes = words => {
+    let currentLength = 0; // There won't be any!
+    const wordCount = words.length;
+    let currentWords = [];
+    let boxes = [];
+
+    for (let i = 0; i < wordCount; ++i) {
+      if (words[i].length !== currentLength) {
+        if (currentWords.length !== 0) {
+          boxes.push(<WordBox key={currentLength} words={currentWords} />);
+        }
+
+        currentWords = [];
+        currentLength = words[i].length;
+      }
+
+      currentWords.push(words[i]);
+    }
+
+    return boxes.concat(<WordBox key={currentLength} words={currentWords} />);
+  };
+
   render() {
     const { words, loading } = this.props;
 
@@ -9,6 +35,8 @@ class WordView extends React.Component {
     if (loading) {
       return <h2 className="wordview-title">Loading...</h2>;
     }
+
+    return this.renderBoxes(words);
   }
 }
 
