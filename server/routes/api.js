7f99046d3ca64e157644d05ac6c69f32d0817f99
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-17 20:56:43 +0000

    /api/words returns the actual words that can be built

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/words', (req, res) => {
-  console.log('/api/words:', req.body.letters);
+const WordList = require('../../src/WordList');
 
-  res.json({ words: ['bellini', 'abacus', 'wings'] });
+const wordlist = new WordList('../public/countdown.words');
+console.log(wordlist.words.length);
+
+router.post('/words', (req, res) => {
+  const words = wordlist.canBeMadeFrom(req.body.letters);
+
+  res.json({ words });
 });
 
 module.exports = {
