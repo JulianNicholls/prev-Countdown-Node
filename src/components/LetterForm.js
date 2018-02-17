f0017280a19f4d2a6e378b1c8b7b91a33758eeae
Julian Nicholls <juliannicholls29@gmail.com>
2018-02-17 10:48:27 +0000

    Add countdown button and style form.

--- src/components/LetterForm.js
+++ src/components/LetterForm.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
 class LetterForm extends React.Component {
+  state = {
+    letters: ''
+  };
+
+  updateLetters = event => {
+    const letters = event.target.value;
+    this.setState(() => ({ letters }));
+  };
+
+  getWords = event => {
+    event.preventDefault();
+
+    this.props.getWords(this.state.letters);
+  };
+
   render() {
     return (
       <form>
-        <input type="text" id="letters" />
+        <input
+          type="text"
+          id="letters"
+          value={this.state.letters}
+          onChange={this.updateLetters}
+        />
+        <button
+          className="big-button"
+          onClick={this.getWords}
+          disabled={this.state.letters.length < 8}
+        >
+          Countdown
+        </button>
       </form>
     );
   }
