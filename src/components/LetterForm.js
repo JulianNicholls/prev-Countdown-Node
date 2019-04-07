ef610ee08eb0772dc26f4ebf2bf0fe3137eacb38
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-07 20:56:01 +0100

    Re-factor LetterForm and adjust styles.

--- src/components/LetterForm.js
+++ src/components/LetterForm.js
@@ -1,47 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-class LetterForm extends React.Component {
-  state = {
-    letters: '',
-  };
+const LetterForm = ({ getWords }) => {
+  const [letters, setLetters] = useState('');
 
-  updateLetters = evt => {
-    this.setState({ letters: evt.target.value });
-  };
-
-  getWords = event => {
+  const findWords = event => {
     event.preventDefault();
 
-    this.props.getWords(this.state.letters);
+    getWords(letters);
 
     return false;
   };
 
-  render() {
-    const { letters } = this.state;
+  return (
+    <div className="container">
+      <form onSubmit={findWords}>
+        <input
+          type="search"
+          id="letters"
+          value={letters}
+          onChange={event => setLetters(event.target.value)}
+          autoComplete="off"
+          autoCorrect="off"
+          autoFocus
+        />
+        <button
+          type="submit"
+          className="big-button"
+          disabled={letters.length < 7}
+        >
+          Countdown
+        </button>
+      </form>
+    </div>
+  );
+};
 
-    return (
-      <div className="container">
-        <form onSubmit={this.getWords}>
-          <input
-            type="search"
-            id="letters"
-            value={letters}
-            onChange={this.updateLetters}
-            autoComplete="off"
-            autoCorrect="off"
-          />
-          <button
-            type="submit"
-            className="big-button"
-            disabled={letters.length < 7}
-          >
-            Countdown
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+LetterForm.propTypes = {
+  getWords: PropTypes.func.isRequired,
+};
 
 export default LetterForm;
